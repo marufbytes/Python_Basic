2f1598ddbfdd4f0b8adac61ebe235f23c272b077
Maruf Ahammed <marufahammed215@gmail.com>
2025-02-15 11:58:58 +0600

    Git

---
+++ Basic/Arithmatic_Operator.py
@@ -0,0 +1,11 @@
+print('Addition',10+3)
+print('Subtraction',10-3)
+print('Multiplication',10*3)
+print('Normal Division',10/3)
+print('Int Division',10//3)
+print('M Perchant',10%3)
+print('Exponent', 10**3)
+
+x=10
+x=x+3
+print('x : ' , +x)
