5ac6e2206f2eaf2d3749852f6eabea8771795e11
Maruf Ahammed <marufahammed215@gmail.com>
2025-02-15 20:02:55 +0600

    Git

---
+++ Basic/If_Statement.py
@@ -0,0 +1,15 @@
+is_hot = False
+is_cold=True
+
+if is_hot : 
+    print("It's a hot day . ")
+    print("Drink plenty of water ")
+
+if is_cold:
+    print("It's a cold day.")
+    print("Wear warm clothes.")
+
+else:
+    print("Enjoy your day !")
+
+
