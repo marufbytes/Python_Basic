d2e02e3b989c716ffab99e353e93efc2bcb5cf83
Maruf Ahammed <marufahammed215@gmail.com>
2025-02-20 19:52:24 +0600

    Parameter

--- Basic/Parameterized_Function.py
+++ Basic/Parameterized_Function.py
@@ -1,9 +1,12 @@
-def greet_user (name ):
-    print(f'Hi {name}!')
+def greet_user (first_name, last_name ):
+    print(f'Hi {first_name}{last_name}!')
     print('Welcome abroad!')
 
 
 print("Start!")
-greet_user("Maruf")
-greet_user("Ahmed")
-print("Finish!")
+print('\n')
+greet_user("Maruf", " Ahammed")
+print('\n')
+greet_user(last_name="Syed",first_name="Nafiul ")
+print('\n')
+print("Finish..............")
