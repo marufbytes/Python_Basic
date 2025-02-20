c39bdefed5ee20748d74283e58852e2530b957a1
Maruf Ahammed <marufahammed215@gmail.com>
2025-02-20 21:57:26 +0600

    Git
    Git

---
+++ Basic/Constructor.py
@@ -0,0 +1,17 @@
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        
+    def move (self):
+        print("Move")
+
+    def draw(self):
+        print('Draw')
+
+
+point = Point()
+
+
+
+    
