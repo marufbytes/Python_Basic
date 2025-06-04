9a70571904ac6b79c0456b09aa3d84007a6c2466
Maruf Ahammed <marufahammed215@gmail.com>
2025-06-04 22:22:06 +0600

    Git

---
+++ Apna_College/Third Lecture/List.py
@@ -0,0 +1,12 @@
+marks = [94.4, 87.5, 56.6, 65.5, 86.5, 67.4]
+print(marks)
+print(type(marks))
+
+student = ["Maruf", 98.4, 17, "Cumilla"]
+print(student)
+print(student[0])
+student[0] = "Mashruf"
+print(student[0])  # Undate possible in case of list, but not for string
+print(student)
+
+print(marks[3:6])
