d9df43081615dbe692c1424be18b45b1dfed4895
Maruf Ahammed <marufahammed215@gmail.com>
2025-06-04 09:43:37 +0600

    Git

---
+++ Apna_College/Second Lecture/Condition.py
@@ -0,0 +1,10 @@
+age = int(input("Enter age : "))
+
+if (age > 18):
+    print("Eligible for NID")
+
+elif (age == 18):
+    print("Also eligible.")
+
+else:
+    print("Ain't eligible")
---
+++ Apna_College/Second Lecture/Count.py
@@ -0,0 +1,2 @@
+str = "$ Hi , this is me $ Maruf$ and price is $43.54"
+print(str.count("$"))
