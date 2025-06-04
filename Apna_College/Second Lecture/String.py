9ec3fde18c5ec6f6b71f17c4a8f0ad473547706c
Maruf Ahammed <marufahammed215@gmail.com>
2025-06-04 09:32:43 +0600

    Git

--- Apna_College/Second Lecture/String.py
+++ Apna_College/Second Lecture/String.py
@@ -1,3 +1,32 @@
-str1="This is a stirng"
-str2 = 'MarufAhammed'
-str3
+str1="this is a string"
+str2 = 'Maruf Ahammed'
+str3 = """This is a string """
+str4= "This is a  string.\nWe are creating it in python."
+print(str4)
+
+str5 = "Thsi is a string.\t We are doing it in python."
+print(str5)
+
+#Concatenation
+print(str1+str2)
+str6 =(str1+" "+str2)
+print(str6)
+
+#Lentgth
+print(len(str1+str2))
+print(len(str6))
+
+#index
+ch=str1[0]
+print(ch)
+
+#Slicing
+ch2=str1[0:6]
+print(ch2)
+print(str1[:4]) #0:4
+print((str1[5:])) 
+print(str1[-4:-1])
+
+#Key word
+print(str1.endswith("ng"))
+print(str1.capitalize())
