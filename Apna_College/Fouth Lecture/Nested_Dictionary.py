79597423709b22f597ef6d2a89151f1501e14cb0
Maruf Ahammed <marufahammed215@gmail.com>
2025-06-06 19:24:12 +0600

    Git

---
+++ Apna_College/Fouth Lecture/Nested_Dictionary.py
@@ -0,0 +1,13 @@
+student = {
+    "name": "Maruf Ahmed",
+    "class": 11,
+
+    "marks": {
+        "phy": 97,
+        "chem": 87,
+        "math": 76
+    }
+}
+
+print(student["marks"])
+print(student)
