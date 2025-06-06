7a4a650837191fcddb5d3ebc7378f0b3fe4c6af5
Maruf Ahammed <marufahammed215@gmail.com>
2025-06-06 08:30:28 +0600

    Git

---
+++ Apna_College/Fouth Lecture/Dictionary.py
@@ -0,0 +1,19 @@
+info = {
+    "key": "value",
+    "name": "Maruf",
+    "learning": ("python, C++, C"),
+    "age": 22,
+    "varsity": True,
+    "marks": 45.34
+}
+
+print(info)
+print(info["name"])
+print(info["age"])
+print(info["learning"])
+
+info["name"] = "Maruf Ahmedd"  # update
+print(info)
+
+nullDictionary = {}
+print("Null Dictionary : ", nullDictionary)
---
+++ Apna_College/Fouth Lecture/tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+info = {
+    "key": "value",
+    "name": "Maruf",
+    "learning": ("python, C++, C"),
+    "age": 22,
+    "varsity": True,
+    "marks": 45.34
+}
+
+print(info)
+print(type(info))
+print(info["age"])
+print(info["learning"])
+print(info["name"])
+
+info["name"] = "Maruf Ahmedd"
+print(info)
