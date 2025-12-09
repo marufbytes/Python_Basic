c922fa4fd85c59404e43c34c02788a3a4536a394
marufbytes <marufahammed215@gmail.com>
2025-12-10 01:01:09 +0600

    Git

---
+++ Hackerrank/Week_1/Day_1/Intro.py
@@ -0,0 +1,6 @@
+print("Hi there! I'm Maruf Ahammed from department of CSE. My goal to complete tomorrow's mission.")
+
+print("This is Maruf from AIUB. Labib is my Guru. My cg is much less than Labib.\n"
+      "This is task from my guru. He ased to me complete this work within today and submit to him \n"
+      "He also mentioned to upload it to github,\n"
+      "And I'm following him")
