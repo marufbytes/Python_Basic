3420ecd89ccb7fe66fba77bc7f0ecfd49e1ebb7e
marufbytes <marufahammed215@gmail.com>
2025-12-10 00:48:49 +0600

    Git

---
+++ Hachathon/Variables.py
@@ -0,0 +1,16 @@
+age =25
+price = 19.53
+name = "Maruf Ahemmed"
+flowers = ["roe", "Lily", "Tulip"]
+
+student = {
+    "name": "Maruf Ahammed",
+    "age": 21,
+    "grade": "A"
+}
+
+print (age)
+print (name)
+print(flowers)
+print(price)
+print (student)
