47c03b538243dafb670290a41ca59aefc9c95a9e
marufbytes <marufahammed496@gmail.com>
2025-12-11 21:43:48 +0600

    Git

---
+++ Hackathon/Week_1/Day_2/If_Else.py
@@ -0,0 +1,9 @@
+num = float(input("Enter a number: "))
+if num>0:
+    print("Positive")
+
+elif num<0:
+    print("Negatine")
+
+else:
+    print("Zero")
---
+++ Hackathon/Week_1/Day_2/If_Else_Array.py
@@ -0,0 +1,8 @@
+num = [1,3,5,6,6,7,8,9,7,4,5]
+
+for i in num:
+    if i>5:
+        print("Greater than 5")
+
+    else:
+        print("Less thant five")
