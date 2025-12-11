8d6597b2786e3eb55c9789c6ee5dccb045bd6011
marufbytes <marufahammed215@gmail.com>
2025-12-12 02:53:00 +0600

    Git

---
+++ Hackathon/Week_1/Day_3/For.py
@@ -0,0 +1,4 @@
+name = ["Maruf","Sajid","Shohag","Sadib"]
+
+for i in name:
+    print(i)
---
+++ Hackathon/Week_1/Day_3/While.py
@@ -0,0 +1,6 @@
+name = ["Maruf","Sadib","Sajid","Shohag"]
+
+i=0
+while i<len(name):
+    print(name[i])
+    i+=1
